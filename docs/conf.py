accff9f0152012f0f4ecbd743d21ea294224fd61
Adam Trigui <a.trigui@ip2i.in2p3.fr>
2026-01-27 17:27:26 +0100

    same correction

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ author = 'Adam Trigui'
 try:
     release = importlib.metadata.version("ximinf")  # Get version from package
 except importlib.metadata.PackageNotFoundError:
-    release = "0.0.41" # Fallback version if package not found
+    release = "0.0.42" # Fallback version if package not found
 
 # Short X.Y version
 version = ".".join(release.split(".")[:2])
