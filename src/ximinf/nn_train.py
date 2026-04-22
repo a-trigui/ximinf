cfdb8b8677d9c350a772cd6dd873ba3edabb7c5a
Adam Trigui <a.trigui@ip2i.in2p3.fr>
2026-04-22 10:53:15 +0200

    Taking off bias before layer normalisation

--- src/ximinf/nn_train.py
+++ src/ximinf/nn_train.py
@@ -223,7 +223,7 @@ def pred_step(model, x_batch):
 
 class Embedding(nnx.Module):
     def  __init__(self, Nsize, n_params, *, rngs):
-        self.linear1 = nnx.Linear(n_params, Nsize, use_bias=True, rngs=rngs)
+        self.linear1 = nnx.Linear(n_params, Nsize, use_bias=False, rngs=rngs)
         self.ln1     = nnx.LayerNorm(Nsize, rngs=rngs)
         self.linear2 = nnx.Linear(Nsize, Nsize, use_bias=True, rngs=rngs)
 
@@ -238,13 +238,13 @@ class Embedding(nnx.Module):
 
 class Phi(nnx.Module):
     def __init__(self, Nsize, n_cols, n_params, *, rngs):
-        self.linear1 = nnx.Linear(n_cols + n_params, 2*Nsize, use_bias=True, rngs=rngs)
+        self.linear1 = nnx.Linear(n_cols + n_params, 2*Nsize, use_bias=False, rngs=rngs)
         self.ln1     = nnx.LayerNorm(2*Nsize, rngs=rngs)
-        self.linear2 = nnx.Linear(2*Nsize, 2*Nsize, use_bias=True, rngs=rngs)
+        self.linear2 = nnx.Linear(2*Nsize, 2*Nsize, use_bias=False, rngs=rngs)
         self.ln2     = nnx.LayerNorm(2*Nsize, rngs=rngs)
-        self.linear3 = nnx.Linear(2*Nsize, 2*Nsize, use_bias=True, rngs=rngs)
+        self.linear3 = nnx.Linear(2*Nsize, 2*Nsize, use_bias=False, rngs=rngs)
         self.ln3     = nnx.LayerNorm(2*Nsize, rngs=rngs)
-        self.linear4 = nnx.Linear(2*Nsize, Nsize, rngs=rngs)
+        self.linear4 = nnx.Linear(2*Nsize, Nsize, use_bias=True, rngs=rngs)
 
     def __call__(self, data, params):
         h = jnp.concatenate([data, params], axis=-1)
@@ -271,13 +271,13 @@ class Rho(nnx.Module):
     with separate LayerNorm for pooled features and theta.
     """
     def __init__(self, Nsize_p, Nsize_r, Nsize_e, *, rngs):
-        self.linear1 = nnx.Linear(Nsize_p + Nsize_e + 1, Nsize_r, use_bias=True, rngs=rngs)
+        self.linear1 = nnx.Linear(Nsize_p + Nsize_e + 1, Nsize_r, use_bias=False, rngs=rngs)
         self.ln1     = nnx.LayerNorm(Nsize_r, rngs=rngs)
-        self.linear2 = nnx.Linear(Nsize_r, Nsize_r, use_bias=True, rngs=rngs)
+        self.linear2 = nnx.Linear(Nsize_r, Nsize_r, use_bias=False, rngs=rngs)
         self.ln2     = nnx.LayerNorm(Nsize_r, rngs=rngs)
-        self.linear3 = nnx.Linear(Nsize_r, Nsize_r, use_bias=True, rngs=rngs)
+        self.linear3 = nnx.Linear(Nsize_r, Nsize_r, use_bias=False, rngs=rngs)
         self.ln3     = nnx.LayerNorm(Nsize_r, rngs=rngs)
-        self.linear4 = nnx.Linear(Nsize_r, 1, rngs=rngs)
+        self.linear4 = nnx.Linear(Nsize_r, 1, use_bias=True, rngs=rngs)
 
     def __call__(self, dropout, pooled_features, params):
         x = jnp.concatenate([pooled_features, params], axis=-1)
