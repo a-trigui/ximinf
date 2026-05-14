2834dc13bb329a1d733fdfaed00e1019d41fc34a
Adam Trigui <a.trigui@ip2i.in2p3.fr>
2026-05-14 11:30:47 +0200

    Chaning version

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ author = 'Adam Trigui'
 try:
     release = importlib.metadata.version("ximinf")  # Get version from package
 except importlib.metadata.PackageNotFoundError:
-    release = "0.0.124" # Fallback version if package not found
+    release = "0.0.125" # Fallback version if package not found
 
 # Short X.Y version
 version = ".".join(release.split(".")[:2])
